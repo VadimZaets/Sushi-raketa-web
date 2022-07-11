dd0004b154a3fd3d4f74b2a9a5c619d2f9f2cbf5
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2022-07-11 22:19:09 +0300

    add pic

--- src/pages/MainView/MainView.js
+++ src/pages/MainView/MainView.js
@@ -1,7 +1,13 @@
+import Carousel from "../../components/Carousel/Carousel";
 import Header from "../../components/Header/Header";
 
 const MainView = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Carousel />
+    </>
+  );
 };
 
 export default MainView;
